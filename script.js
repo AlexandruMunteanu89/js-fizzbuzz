88fc78f5b51265ad352e02309f5f4d504743d6a8
AlexandruMunteanu89 <sasamunteanu18@gmail.com>
2026-02-28 22:30:37 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+for (let i = 1; i <= 100; i++) {
+    //controlliamo se il numero è divisibile per 3 e 5
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else {
+        console.log(i);
+    };
+}
