df1db2fe81f4686a3b016d5badcb3fb2fd213d06
AlexandruMunteanu89 <sasamunteanu18@gmail.com>
2026-02-28 22:33:22 +0100

    aggiunto la seconda condizione

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ for (let i = 1; i <= 100; i++) {
     //controlliamo se il numero è divisibile per 3 e 5
     if (i % 3 === 0 && i % 5 === 0) {
         console.log("FizzBuzz");
+    }
+    //controlliamo se il numero è divisibile per 3
+    else if (i % 3 === 0) { 
+        console.log("Fizz");
     } else {
         console.log(i);
     };
